1fa6763f106abf16e0840ad9cf93dcdff946e64d
rajatsaini1 <rajatsaini0777@gmail.com>
2022-02-07 13:10:36 +0530

    EC2 added

--- main.tf
+++ main.tf
@@ -12,7 +12,7 @@ terraform {
   required_version = ">= 1.1.0"
 
   cloud {
-    organization = "REPLACE_ME"
+    organization = "rajats"
 
     workspaces {
       name = "gh-actions-demo"
@@ -22,35 +22,13 @@ terraform {
 
 
 provider "aws" {
-  region = "us-west-2"
+  region = "ap-south-1"
 }
 
 
-
-resource "random_pet" "sg" {}
-
-resource "aws_instance" "web" {
-  ami                    = "ami-830c94e3"
-  instance_type          = "t2.micro"
-  vpc_security_group_ids = [aws_security_group.web-sg.id]
-
-  user_data = <<-EOF
-              #!/bin/bash
-              echo "Hello, World" > index.html
-              nohup busybox httpd -f -p 8080 &
-              EOF
+resource "aws_instance" "myec2" {
+    ami = "ami-0851b76e8b1bce90b"
+    instance_type = "t2.micro"
+  
 }
 
-resource "aws_security_group" "web-sg" {
-  name = "${random_pet.sg.id}-sg"
-  ingress {
-    from_port   = 8080
-    to_port     = 8080
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-}
-
-output "web-address" {
-  value = "${aws_instance.web.public_dns}:8080"
-}
